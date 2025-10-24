f04a1be34d3b7403c28f45bff0cb5a8a57cda336
Taha Aziz <tahaaziz73@gmail.com>
2025-10-24 12:26:48 -0400

    typos

--- cli.js
+++ cli.js
@@ -13,7 +13,6 @@ program
     .action((file) => {
         const logFile = 'turbo.json';
         const nodeCommand = `node --trace-opt --trace-deopt --redirect-code-traces --redirect-code-traces-to=${logFile} ${file}`;
-
         console.log(`Generating V8 logs for "${file}"...`);
 
         // Execute the node command to generate the log file
@@ -24,7 +23,7 @@ program
             }
             console.log('Logs generated successfully!');
 
-            console.log('Starting visualization server...');
+            console.log('Starting local visualization server...');
 
             // Spawn the server process
             const serverPath = path.join(__dirname, 'server.js');
